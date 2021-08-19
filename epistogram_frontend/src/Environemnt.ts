391ec8f8d34558a1459349cdafeeeb477ddd215f
Bence Manyoki <manyoki.bence@epistogram.com>
2021-08-19 20:08:06 +0200

    Envirnment stuff.

--- epistogram_frontend/src/Environemnt.ts
+++ epistogram_frontend/src/Environemnt.ts
@@ -1,7 +1,8 @@
-export const isLocalhost = !!process.env.IS_DEPLOYED_ENVIRONMENT;
+export const isLocalhost = process.env.IS_DEPLOYED_ENVIRONMENT ? false : true;
 
 export const backendUrl = isLocalhost
-    ? window.location.origin + "/api/"
-    : "localhost:5000/"
+    ? "localhost:5000/"
+    : window.location.origin + "/api/"
 
+console.log(process.env.IS_DEPLOYED_ENVIRONMENT + " - " + isLocalhost);
 console.log(isLocalhost ? "Running on localhost." : "Running deployed to an environment.");
