25e3a5a408bbdda2b6f95140f2ef0bcfdd2da543
Bence Manyoki <manyoki.bence@epistogram.com>
2021-08-19 20:20:14 +0200

    Modified env vars.

--- epistogram_frontend/src/Environemnt.ts
+++ epistogram_frontend/src/Environemnt.ts
@@ -1,8 +1,8 @@
-export const isLocalhost = process.env.IS_DEPLOYED_ENVIRONMENT ? false : true;
+export const isLocalhost = process.env.REACT_APP_IS_DEPLOYED ? false : true;
 
 export const backendUrl = isLocalhost
     ? "localhost:5000/"
     : window.location.origin + "/api/"
 
-console.log(process.env.IS_DEPLOYED_ENVIRONMENT + " - " + isLocalhost);
+console.log(process.env.REACT_APP_IS_DEPLOYED + " - " + isLocalhost);
 console.log(isLocalhost ? "Running on localhost." : "Running deployed to an environment.");
