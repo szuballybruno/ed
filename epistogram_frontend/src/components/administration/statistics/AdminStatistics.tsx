3e2df7e51afdc88fa613032b4f492c817768860a
Manfréd Spengler <spengler.manfred@epistogram.com>
2021-10-12 19:59:52 +0200

    Quick fix for charts

--- epistogram_frontend/src/components/administration/statistics/AdminStatistics.tsx
+++ epistogram_frontend/src/components/administration/statistics/AdminStatistics.tsx
@@ -187,7 +187,7 @@ const AdminStatistics = () => {
                                     display: false
                                 }
                             },
-                            scale: {
+                            scales: {
                                 min: 0,
                                 max: 7
                             }
