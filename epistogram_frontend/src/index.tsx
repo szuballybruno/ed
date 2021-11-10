371836d11b32faa0bc266f4d17027f8743f95561
Bence Manyoki <manyoki.bence@epistogram.com>
2021-11-10 16:23:23 +0100

    asd

--- epistogram_frontend/src/index.tsx
+++ epistogram_frontend/src/index.tsx
@@ -9,26 +9,47 @@ import { ErrorDialogFrame } from "./components/HOC/DialogFrame";
 import { NotificationsFrame } from "./components/HOC/NotificationsFrame";
 import { UnderMaintanence } from "./components/UnderMaintanence";
 import { applicationRoutes } from "./configuration/applicationRoutes";
-import { theme } from "./configuration/defaultMUITheme";
 import { isUnderMaintenance } from "./Environemnt";
 import './index.css';
 import './jsExtensions.ts'; // extensions, important
 import { MainRouting } from "./MainRouting";
 import { extendTheme, ThemeConfig } from "@chakra-ui/react"
+import { createTheme } from "@mui/system";
 
+// react query 
 const queryClient = new QueryClient();
 
+// chakra theme
 const config: ThemeConfig = {
     initialColorMode: "light",
     useSystemColorMode: false,
 }
 const chakraTheme = extendTheme({ config })
 
+// mui theme
+const muiTheme = createTheme({
+    palette: {
+        primary: {
+            light: '#c8e8ff',
+            main: '#97c9cc',
+            dark: '#c8e8ff',
+            contrastText: '#000000',
+        },
+        secondary: {
+            light: '#5495b4',
+            main: "#1d6784",
+            dark: '#5495b4',
+            contrastText: '#fff',
+        }
+
+    }
+})
+
 ReactDOM.render(
     <BrowserRouter getUserConfirmation={(msg, callback) => { console.log("What??") }}>
         <QueryClientProvider client={queryClient}>
             <ChakraProvider theme={chakraTheme}>
-                <ThemeProvider theme={theme}>
+                <ThemeProvider theme={muiTheme}>
                     <Switch>
 
                         {/* under maintanence */}
