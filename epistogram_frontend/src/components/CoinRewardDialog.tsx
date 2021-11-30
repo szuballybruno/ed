d12204a9bb212b25b24b096a7f3df3931f3b6727
Bence Manyoki <manyoki.bence@epistogram.com>
2021-11-30 12:58:59 +0100

    asd

---
+++ epistogram_frontend/src/components/CoinRewardDialog.tsx
@@ -0,0 +1,68 @@
+import { Box } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/react";
+import { Player } from "@lottiefiles/react-lottie-player";
+import { Typography } from "@mui/material";
+import { getAssetUrl } from "../frontendHelpers";
+import { EpistoDialog, EpistoDialogLogicType } from "./EpistoDialog";
+import { EpistoButton } from "./universal/EpistoButton";
+
+export const CoinRewardDialog = (props: {
+    coinRewardAmount: number,
+    text: string,
+    lottiePath: string,
+    dialogLogic: EpistoDialogLogicType
+}) => {
+
+    const { dialogLogic, lottiePath, coinRewardAmount, text } = props;
+
+    return <EpistoDialog logic={dialogLogic}>
+        <Flex
+            direction="column"
+            borderRadius="5px"
+            border="solid 5px var(--epistoTeal)"
+            align="center"
+            justify="center"
+            pb="40px"
+            px="80px">
+
+            <Box
+                pos="relative"
+                width="300px"
+                height="300px">
+
+                <Player
+                    autoplay
+                    loop
+                    src={getAssetUrl(lottiePath)}
+                    style={{ position: "absolute", width: "100%", height: "100%" }} />
+            </Box>
+
+            <Typography className="fontColorSecondary" noWrap>
+                {text}
+            </Typography>
+
+            <Flex align="flex-end" mt="10px">
+                <Typography className="fontGiant" style={{ color: "var(--epistoTeal)", fontWeight: "bold" }}>
+                    {coinRewardAmount}
+                </Typography>
+
+                <Typography className="fontColorSecondary" style={{ margin: "0px 0px 4px 4px" }}>
+                    EpistoCoin
+                </Typography>
+
+                <img
+                    src={getAssetUrl("images/epistoCoin.png")}
+                    className="square25"
+                    style={{ margin: "0px 0px 4px 4px" }} />
+
+                <EpistoButton
+                    variant="colored"
+                    style={{ margin: "0px 0px 0px 15px" }}
+                    onClick={() => dialogLogic.closeDialog()}>
+
+                    Begyűjtöm!
+                </EpistoButton>
+            </Flex>
+        </Flex>
+    </EpistoDialog>
+}
--- epistogram_frontend/src/components/system/EventListener.tsx
+++ epistogram_frontend/src/components/system/EventListener.tsx
@@ -1,12 +1,8 @@
-import { Box } from "@chakra-ui/layout";
-import { Flex } from "@chakra-ui/react";
-import { Player } from "@lottiefiles/react-lottie-player";
-import { Typography } from "@mui/material";
-import { ReactNode, useEffect, useState } from "react"
-import { getAssetUrl } from "../../frontendHelpers";
+import { ReactNode, useEffect, useState } from "react";
 import { EventCoinAcquireNotificationDTO } from "../../models/shared_models/EventCoinAcquireNotificationDTO";
 import { useEventListener } from "../../services/eventService";
-import { EpistoDialog, useEpistoDialogLogic } from "../EpistoDialog";
+import { CoinRewardDialog } from "../CoinRewardDialog";
+import { useEpistoDialogLogic } from "../EpistoDialog";
 
 export const EventListener = (props: { children: ReactNode }) => {
 
@@ -15,6 +11,8 @@ export const EventListener = (props: { children: ReactNode }) => {
     const { event } = useEventListener();
     const dialogLogic = useEpistoDialogLogic({ hideDefaultCloseButton: true });
     const [coinRewardAmount, setCoinRewardAmount] = useState(0);
+    const [coinRewardLottie, setCoinRewardLottie] = useState("");
+    const [coinRewardText, setCoinRewardText] = useState("");
 
     useEffect(() => {
 
@@ -25,7 +23,25 @@ export const EventListener = (props: { children: ReactNode }) => {
 
             const coinAcEvent = event.data as EventCoinAcquireNotificationDTO;
 
+            // set dialog props 
             setCoinRewardAmount(coinAcEvent.amount);
+
+            if (coinAcEvent.reason === "activity_streak_3_days") {
+
+                setCoinRewardLottie("lottie_json/session_streak_3.json");
+                setCoinRewardText("3 egymást követő napon is beléptél, jutalmad pedig");
+            }
+
+            if (coinAcEvent.reason === "activity_streak_5_days") {
+                setCoinRewardLottie("lottie_json/session_streak_5.json");
+                setCoinRewardText("5 egymást követő napon is beléptél, jutalmad pedig");
+            }
+
+            if (coinAcEvent.reason === "activity_streak_10_days") {
+                setCoinRewardLottie("lottie_json/session_streak_10.json");
+                setCoinRewardText("10 egymást követő napon is beléptél, jutalmad pedig");
+            }
+
             dialogLogic.openDialog();
         }
 
@@ -33,42 +49,11 @@ export const EventListener = (props: { children: ReactNode }) => {
 
     return <>
 
-        <EpistoDialog logic={dialogLogic}>
-            <Flex
-                direction="column"
-                borderRadius="5px"
-                border="solid 5px var(--epistoTeal)"
-                align="center"
-                justify="center"
-                pb="40px">
-
-                <Box
-                    pos="relative"
-                    width="300px"
-                    height="300px">
-
-                    <Player
-                        autoplay
-                        loop
-                        src={getAssetUrl("lottie_json/session_streak_3.json")}
-                        style={{ position: "absolute", width: "100%", height: "115%" }} />
-                </Box>
-
-                <Typography className="fontSmall fontColorSecondary">
-                    {coinRewardAmount} egymást követő kérdésre válaszoltál helyes, jutalmad pedig
-                </Typography>
-
-                <Flex align="flex-end">
-                    <Typography className="fontGiant" style={{ color: "var(--epistoTeal)", fontWeight: "bold" }}>
-                        {coinRewardAmount}
-                    </Typography>
-
-                    <Typography className="fontColorSecondary" style={{ margin: "0px 0px 4px 4px" }}>
-                        EpistoCoin
-                    </Typography>
-                </Flex>
-            </Flex>
-        </EpistoDialog>
+        <CoinRewardDialog
+            lottiePath={coinRewardLottie}
+            coinRewardAmount={coinRewardAmount}
+            dialogLogic={dialogLogic}
+            text={coinRewardText} />
 
         {children}
     </>
