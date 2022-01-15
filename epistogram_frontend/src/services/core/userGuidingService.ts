258d58a7d3321a1b411243aaa71cb74e71936c73
Bence Manyoki <manyoki.bence@epistogram.com>
2022-01-15 18:11:32 +0100

    user guide help tina

---
+++ epistogram_frontend/src/services/core/userGuidingService.ts
@@ -0,0 +1,11 @@
+const getUserGuiding = () => (window as any).userGuiding; 
+
+export const startUserGuide = () => {
+
+    getUserGuiding().previewGuide(38873);
+}
+
+export const startUserGuideHelp = () => {
+
+    getUserGuiding().expandResourceCenter();
+}
