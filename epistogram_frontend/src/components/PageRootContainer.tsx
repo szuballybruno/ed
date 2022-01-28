4fa63de359033293c038949477501d499b7e85d2
Spengler Manfréd <spengler.manfred@epistogram.com>
2022-01-28 11:13:49 +0100

    gradientBackgroundGenerator created

--- epistogram_frontend/src/components/PageRootContainer.tsx
+++ epistogram_frontend/src/components/PageRootContainer.tsx
@@ -1,5 +1,8 @@
 import { Flex, FlexProps, Image } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
+import { gradientBackgroundGenerator } from '../services/core/gradientBackgroundGenerator';
+import { getRandomInteger, iterate } from '../static/frontendHelpers';
+import { EpistoGrid } from './controls/EpistoGrid';
 
 export const PageRootContainer = (props: {
     children: ReactNode,
@@ -10,10 +13,9 @@ export const PageRootContainer = (props: {
 
     const { children, noMaxWidth, noBackground, backgoundImageSrc, ...css } = props;
 
+    const gradients = gradientBackgroundGenerator()
+
     return <Flex
-        background={backgoundImageSrc || noBackground
-            ? undefined
-            : "var(--gradientBlueBackground)"}
         id="pageRootContainer"
         maxWidth={noMaxWidth ? undefined : "1920px"}
         margin="0 auto"
@@ -21,6 +23,32 @@ export const PageRootContainer = (props: {
         overflow="hidden"
         className="whall"
         {...css}>
+
+        {/* 3x3 Gradient Grid */}
+        {backgoundImageSrc || noBackground
+            ? undefined
+            : <EpistoGrid
+                bgColor={"white"}
+                position="absolute"
+                top={0}
+                w="100vw"
+                h="100vh"
+                zIndex="-1"
+                filter="blur(50px)"
+                minColumnWidth={'33%'}
+                gap={"0"}
+                gridTemplateColumns="repeat(3, 1fr)"
+                auto={'fill'}>
+
+                {gradients.map((gradient) => {
+                    return <Flex
+                        padding="20px"
+                        filter="blur(8px)"
+                        background={gradient}>
+
+                    </Flex>
+                })}
+            </EpistoGrid>}
 
         {(!noBackground && backgoundImageSrc) && <Image
             position="absolute"
