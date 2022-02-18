1df44c6a825fb1077cd1c6c2da1531b8c73e25ab
Bence Manyoki <manyoki.bence@epistogram.com>
2022-02-18 13:19:43 +0100

    asd

--- misc/syncDTOs.bat
+++ misc/syncDTOs.bat
@@ -1,4 +1,10 @@
 
-@RD /S /Q %~dp0..\epistogram_frontend\src\models\shared_models
-mkdir %~dp0..\epistogram_frontend\src\models\shared_models
-xcopy /s %~dp0..\epistogram_backend\models\shared_models %~dp0..\epistogram_frontend\src\models\shared_models /Y
+
+@REM Remove the shared folder from the target destination 
+@RD /S /Q %~dp0..\epistogram_frontend\src\shared
+
+@REM recreate the shared folder in the target destination 
+mkdir %~dp0..\epistogram_frontend\src\shared
+
+@REM copy shared folder contents to target destination
+xcopy /s %~dp0..\epistogram_backend\shared %~dp0..\epistogram_frontend\src\shared /Y
