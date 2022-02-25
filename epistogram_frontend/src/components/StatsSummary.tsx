530ed1c214e82fcf13c9ba401f4b42c6186ab157
Bence Manyoki <manyoki.bence@epistogram.com>
2022-02-25 09:51:58 +0100

    asd

--- epistogram_frontend/src/components/StatsSummary.tsx
+++ epistogram_frontend/src/components/StatsSummary.tsx
@@ -43,7 +43,7 @@ export const StatsSummary = () => {
             }} >
 
             {userProgressData
-                ? <UserProgressChart />
+                ? <UserProgressChart userProgress={userProgressData} />
                 : <NoProgressChartYet />}
         </FlexFloat>
 
