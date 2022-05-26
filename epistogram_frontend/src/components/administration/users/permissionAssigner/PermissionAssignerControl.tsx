6e186938125dbe3102ac156e038102e22bfb2c45
Bence Manyoki <manyoki.bence@epistogram.com>
2022-05-26 16:10:12 +0200

    asd

--- epistogram_frontend/src/components/administration/users/permissionAssigner/PermissionAssignerControl.tsx
+++ epistogram_frontend/src/components/administration/users/permissionAssigner/PermissionAssignerControl.tsx
@@ -1,10 +1,12 @@
 import { Flex } from '@chakra-ui/react';
+import { Delete } from '@mui/icons-material';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useUserPermissions, useUserRoles } from '../../../../services/api/rolesApiService';
 import { UserPermissionDTO } from '../../../../shared/dtos/role/UserPermissionDTO';
 import { UserRoleDTO } from '../../../../shared/dtos/role/UserRoleDTO';
-import { userRolesEqual } from '../../../../shared/logic/sharedLogic';
+import { userPermissionsEqual, userRolesEqual } from '../../../../shared/logic/sharedLogic';
 import { useHandleAddRemoveItems } from '../../../../static/frontendHelpers';
+import { EpistoIcons } from '../../../../static/EpistoIcons';
 import { EpistoButton } from '../../../controls/EpistoButton';
 import { EpistoDataGrid, GridColumnType } from '../../../controls/EpistoDataGrid';
 import { EpistoLabel } from '../../../controls/EpistoLabel';
@@ -12,68 +14,123 @@ import { LoadingFrame } from '../../../system/LoadingFrame';
 import { useEpistoDialogLogic } from '../../../universal/epistoDialog/EpistoDialogLogic';
 import { AssignPermissionDialog } from './AssignPermissionDialog';
 import { AssignRoleDialog } from './AssignRoleDialog';
+import { EpistoFont } from '../../../controls/EpistoFont';
+
+type StateType = 'new' | 'removed' | 'none';
 
 type RoleRowType = {
     rowId: number,
     name: string;
     roleId: number;
     contextCompanyName: string;
-    state: 'new' | 'none';
+    state: StateType;
     isInherited: boolean;
+    data: UserRoleDTO;
 }
 
 type PermissionRowType = {
     rowId: number,
     permissionCode: string;
-    parentRoleName: string;
+    source: string;
     contextName: string;
-    state: 'new' | 'none';
+    state: StateType;
     isInherited: boolean;
+    data: UserPermissionDTO;
 }
 
-export const PermissionAssignerControl = (props: {
+const AuthItemRowTitle = (props: { title: string, state: StateType }) => (
+    <Flex>
+        <EpistoFont>
+            {props.title}
+        </EpistoFont>
+
+        {props.state === 'new' && <EpistoIcons.New
+            style={{ color: 'var(--mildGreen)' }} />}
+
+        {props.state === 'removed' && <EpistoIcons.DeleteOutline
+            style={{ color: 'var(--mildRed)' }} />}
+    </Flex>
+);
+
+const AuthItemRemoveButton = (props: {
+    canRemove: boolean,
+    canRevive: boolean,
+    remove: () => void,
+    revive: () => void
+}) => (
+    <>
+        {props.canRemove && <EpistoButton
+            onClick={props.remove}>
+            <EpistoIcons.Minus />
+        </EpistoButton>}
+
+        {props.canRevive && <EpistoButton
+            onClick={props.revive}>
+            <EpistoIcons.ArrowCircleUp />
+        </EpistoButton>}
+    </>
+);
+
+const useRolesLogic = (props: {
     userId: number,
-    userCompanyId: number | null,
-    onChange: ({ assignedRoles: UserRoleDTO }) => void
+    onChange: (roles: UserRoleDTO[]) => void
 }) => {
 
-    const { userId, onChange, userCompanyId } = props;
+    const { userId, onChange } = props;
 
     // http
     const { userRoles: rolesFromServer } = useUserRoles(userId);
-    const { userPermissions: permissionsFromServer } = useUserPermissions(userId);
 
     // assigned roles
-    const [userRoles, setUserRoles] = useState<UserRoleDTO[]>([]);
-    const [addAssignedRole, removeAssignedRole] = useHandleAddRemoveItems(userRoles, setUserRoles);
+    const [assignedRoles, setAssignedRoles] = useState<UserRoleDTO[]>([]);
+    const [addAssignedRole, removeAssignedRole] = useHandleAddRemoveItems(assignedRoles, setAssignedRoles);
 
-    // assigned permissions 
-    const [assignedPermissions, setAssignedPermissions] = useState<UserPermissionDTO[]>([]);
-    const [addAssignedPermission, removeAssignedPermission] = useHandleAddRemoveItems(assignedPermissions, setAssignedPermissions);
+    const [removedRoles, setRemovedRoles] = useState<UserRoleDTO[]>([]);
+
+    const assignNewRole = useCallback((dto: UserRoleDTO) => {
+
+        addAssignedRole(dto);
+        onChange(assignedRoles);
+    }, [onChange, addAssignedRole]);
+
+    const removeRole = useCallback((dto: UserRoleDTO, isNew: boolean) => {
+
+        if (isNew) {
+
+            setAssignedRoles(assignedRoles
+                .filter(x => !userRolesEqual(x, dto)));
+        }
+        else {
+
+            setRemovedRoles([...removedRoles, dto]);
+        }
+    }, [assignedRoles, setAssignedRoles, setRemovedRoles, removedRoles]);
+
+    const reviveRole = useCallback((dto: UserRoleDTO) => {
+
+        setRemovedRoles(removedRoles
+            .filter(x => !userRolesEqual(x, dto)));
+    }, [setRemovedRoles, removedRoles]);
 
     const getRoleKey = useCallback((x) => x.rowId, []);
-    const getPermissionKey = useCallback((x) => x.rowId, []);
 
     // init roles
     useEffect(() => {
 
-        setUserRoles(rolesFromServer);
+        setAssignedRoles(rolesFromServer);
     }, [rolesFromServer]);
 
-    // init perms
-    useEffect(() => {
-
-        setAssignedPermissions(permissionsFromServer);
-    }, [permissionsFromServer]);
 
     // dialogs
     const roleDialogLogic = useEpistoDialogLogic(AssignRoleDialog);
-    const permissionDialogLogic = useEpistoDialogLogic(AssignPermissionDialog);
 
-    const roleRows = userRoles
+    const roleRows = assignedRoles
         .map((assRole, i): RoleRowType => {
 
             const foundInServer = rolesFromServer
+                .any(x => userRolesEqual(x, assRole));
+
+            const foundInRemoved = removedRoles
                 .any(x => userRolesEqual(x, assRole));
 
             return {
@@ -81,28 +138,93 @@ export const PermissionAssignerControl = (props: {
                 name: assRole.roleName!,
                 roleId: assRole.roleId,
                 contextCompanyName: assRole.contextCompanyName,
-                state: foundInServer ? 'none' : 'new',
-                isInherited: assRole.isInherited
+                state: foundInRemoved
+                    ? 'removed'
+                    : foundInServer
+                        ? 'none'
+                        : 'new',
+                isInherited: assRole.isInherited,
+                data: assRole
             };
         });
 
-    const permissionRows = assignedPermissions
-        .map((assPerm, i): PermissionRowType => ({
-            rowId: i,
-            permissionCode: assPerm.permissionCode,
-            contextName: (assPerm.contextCompanyName || assPerm.contextCourseName) ?? '',
-            parentRoleName: assPerm.parentRoleName ?? '',
-            state: 'none',
-            isInherited: !!assPerm.parentRoleId
-        }))
-        .orderBy(x => `${x.isInherited}`);
+    const roleColumns = useMemo((): GridColumnType<RoleRowType, any, keyof RoleRowType>[] => [
+        {
+            field: 'name',
+            headerName: 'Name',
+            width: 250,
+            renderCell: ({ value, row }) => (
+                <AuthItemRowTitle
+                    state={row.state}
+                    title={value as string} />
+            )
+        },
+        {
+            field: 'contextCompanyName',
+            headerName: 'Context',
+            width: 150,
+        },
+        {
+            field: 'isInherited',
+            headerName: 'Inherited',
+            width: 150,
+        },
+        {
+            field: 'rowId',
+            headerName: '',
+            width: 50,
+            renderCell: ({ row }) => (
+                <AuthItemRemoveButton
+                    canRemove={!row.isInherited && row.state !== 'removed'}
+                    canRevive={row.state === 'removed'}
+                    remove={() => removeRole(row.data, row.state === 'new')}
+                    revive={() => reviveRole(row.data)} />
+            )
+        }
+    ], [removeRole, reviveRole]);
 
-    const assignNewRole = useCallback((dto: UserRoleDTO) => {
+    const newRoles = useMemo(() => {
 
-        addAssignedRole(dto);
+        return roleRows
+            .filter(x => x.state === 'new')
+            .map(x => x.data);
+    }, [roleRows]);
 
-        // add inherited permissions
-        addAssignedPermission(dto
+    return {
+        assignedRoles: assignedRoles,
+        roleDialogLogic,
+        assignNewRole,
+        roleColumns,
+        getRoleKey,
+        roleRows,
+        newRoles,
+        removedRoles
+    };
+};
+
+// permissions 
+const usePermissionLogic = (props: {
+    userId: number,
+    rolesLogic: ReturnType<typeof useRolesLogic>
+}) => {
+
+    const { userId, rolesLogic } = props;
+
+    // http
+    const { userPermissions: permissionsFromServer } = useUserPermissions(userId);
+
+    // assigned permissions 
+    const [assignedPermissions, setAssignedPermissions] = useState<UserPermissionDTO[]>([]);
+    const [addAssignedPermission, removeAssignedPermission] = useHandleAddRemoveItems(assignedPermissions, setAssignedPermissions);
+    const [removedPermissions, setRemovedPermissions] = useState<UserPermissionDTO[]>([]);
+
+    const permissionDialogLogic = useEpistoDialogLogic(AssignPermissionDialog);
+
+    const getPermissionKey = useCallback((x) => x.rowId, []);
+
+    const newRolePermissions = rolesLogic
+        .newRoles
+        .flatMap(dto => dto
             .permissions
             .map((perm): UserPermissionDTO => ({
                 parentRoleId: dto.roleId,
@@ -117,55 +239,135 @@ export const PermissionAssignerControl = (props: {
                 permissionCode: perm.code
             })));
 
-        onChange({
-            assignedRoles: userRoles
-        });
-    }, [addAssignedPermission, onChange, addAssignedRole]);
+    const removedRolePermissions = rolesLogic
+        .removedRoles
+        .flatMap(dto => dto
+            .permissions
+            .map((perm): UserPermissionDTO => ({
+                parentRoleId: dto.roleId,
+                parentRoleName: dto.roleName,
+                assigneeUserId: userId,
+                contextCompanyId: dto.contextCompanyId,
+                contextCompanyName: dto.contextCompanyName,
+                contextCourseId: null,
+                contextCourseName: null,
+                permissionAssignmentBridgeId: -1,
+                permissionId: perm.id,
+                permissionCode: perm.code
+            })));
+
+    const permissionRows = assignedPermissions
+        .concat(newRolePermissions)
+        .map((assPerm, i): PermissionRowType => ({
+            rowId: i,
+            permissionCode: assPerm.permissionCode,
+            contextName: (assPerm.contextCompanyName || assPerm.contextCourseName) ?? 'User',
+            source: assPerm.parentRoleName ?? 'Assigned',
+            state: (removedPermissions.any(x => userPermissionsEqual(x, assPerm)) || removedRolePermissions.any(x => userPermissionsEqual(x, assPerm)))
+                ? 'removed'
+                : permissionsFromServer.any(x => userPermissionsEqual(x, assPerm))
+                    ? 'none'
+                    : 'new',
+            isInherited: !!assPerm.parentRoleId,
+            data: assPerm,
+        }))
+        .orderBy(x => `${x.data.contextCompanyId}${x.data.contextCourseId}${x.isInherited}`);
 
     const assignNewPermission = useCallback((permissionDTO: UserPermissionDTO) => {
 
-        console.log(permissionDTO);
-    }, []);
+        addAssignedPermission(permissionDTO);
+    }, [addAssignedPermission]);
 
-    const roleColumns = useMemo((): GridColumnType<RoleRowType, any, keyof RoleRowType>[] => [
-        {
-            field: 'name',
-            headerName: 'Name',
-            width: 250
-        },
-        {
-            field: 'contextCompanyName',
-            headerName: 'Context',
-            width: 150,
-        },
-        {
-            field: 'isInherited',
-            headerName: 'Inherited',
-            width: 150,
-        },
-        {
-            field: 'state',
-            headerName: 'State',
+    const removePermission = useCallback((dto: UserPermissionDTO, isNew: boolean) => {
+
+        if (isNew) {
+
+            setAssignedPermissions(assignedPermissions
+                .filter(x => !userPermissionsEqual(x, dto)));
         }
-    ], []);
+        else {
+
+            setRemovedPermissions([...removedPermissions, dto]);
+        }
+    }, [setAssignedPermissions, assignedPermissions, setRemovedPermissions, removedPermissions]);
+
+    const revivePermission = useCallback((dto: UserPermissionDTO) => {
+
+        setRemovedPermissions(removedPermissions
+            .filter(x => !userPermissionsEqual(x, dto)));
+    }, [setRemovedPermissions, removedPermissions]);
 
     const permColumns = useMemo((): GridColumnType<PermissionRowType, any, keyof PermissionRowType>[] => [
         {
             field: 'permissionCode',
             headerName: 'Name',
-            width: 250
+            width: 250,
+            renderCell: ({ value, row }) => (
+                <AuthItemRowTitle
+                    state={row.state}
+                    title={value as string} />
+            )
         },
         {
             field: 'contextName',
             headerName: 'Context',
-            width: 200
+            width: 150
         },
         {
-            field: 'parentRoleName',
-            headerName: 'Inherited from',
-            width: 200
+            field: 'source',
+            headerName: 'Source',
+            width: 150
+        },
+        {
+            field: 'rowId',
+            headerName: '',
+            width: 50,
+            renderCell: ({ row }) => (
+                <AuthItemRemoveButton
+                    canRemove={!row.isInherited && row.state !== 'removed'}
+                    canRevive={!row.isInherited && row.state === 'removed'}
+                    remove={() => removePermission(row.data, row.state === 'new')}
+                    revive={() => revivePermission(row.data)} />
+            )
         }
-    ], []);
+    ], [removePermission, revivePermission]);
+
+    // init perms
+    useEffect(() => {
+
+        setAssignedPermissions(permissionsFromServer);
+    }, [permissionsFromServer]);
+
+    return {
+        permColumns,
+        revivePermission,
+        removePermission,
+        assignNewPermission,
+        permissionRows,
+        permissionDialogLogic,
+        assignedPermissions,
+        getPermissionKey,
+        addAssignedPermission
+    };
+};
+
+export const PermissionAssignerControl = (props: {
+    userId: number,
+    userCompanyId: number | null,
+    onChange: ({ assignedRoles: UserRoleDTO }) => void
+}) => {
+
+    const { userId, onChange, userCompanyId } = props;
+
+    const rolesLogic = useRolesLogic({
+        userId,
+        onChange: (assignedRoles) => onChange({ assignedRoles })
+    });
+
+    const permissionLogic = usePermissionLogic({
+        userId,
+        rolesLogic
+    });
 
     return (
         <LoadingFrame
@@ -174,16 +376,16 @@ export const PermissionAssignerControl = (props: {
 
             {/* assign role */}
             <AssignRoleDialog
-                assignedRoles={userRoles}
-                dialgoLogic={roleDialogLogic}
+                assignedRoles={rolesLogic.assignedRoles}
+                dialgoLogic={rolesLogic.roleDialogLogic}
                 userId={userId}
-                onAdd={assignNewRole} />
+                onAdd={rolesLogic.assignNewRole} />
 
             {/* assign permission */}
             <AssignPermissionDialog
-                dialgoLogic={permissionDialogLogic}
-                assignedPermissions={assignedPermissions}
-                onAdd={assignNewPermission}
+                dialgoLogic={permissionLogic.permissionDialogLogic}
+                assignedPermissions={permissionLogic.assignedPermissions}
+                onAdd={permissionLogic.assignNewPermission}
                 userId={userId} />
 
             <EpistoLabel
@@ -196,7 +398,7 @@ export const PermissionAssignerControl = (props: {
 
                     <EpistoButton
                         variant="colored"
-                        onClick={() => roleDialogLogic.openDialog()}>
+                        onClick={() => rolesLogic.roleDialogLogic.openDialog()}>
 
                         Add
                     </EpistoButton>
@@ -205,9 +407,9 @@ export const PermissionAssignerControl = (props: {
                 <EpistoDataGrid
                     hideFooter
                     density="dense"
-                    columns={roleColumns}
-                    rows={roleRows}
-                    getKey={getRoleKey} />
+                    columns={rolesLogic.roleColumns}
+                    rows={rolesLogic.roleRows}
+                    getKey={rolesLogic.getRoleKey} />
 
             </EpistoLabel>
 
@@ -221,7 +423,7 @@ export const PermissionAssignerControl = (props: {
 
                     <EpistoButton
                         variant="colored"
-                        onClick={() => permissionDialogLogic.openDialog()}>
+                        onClick={() => permissionLogic.permissionDialogLogic.openDialog()}>
 
                         Add
                     </EpistoButton>
@@ -230,9 +432,9 @@ export const PermissionAssignerControl = (props: {
                 <EpistoDataGrid
                     hideFooter
                     density="dense"
-                    columns={permColumns}
-                    rows={permissionRows}
-                    getKey={getPermissionKey} />
+                    columns={permissionLogic.permColumns}
+                    rows={permissionLogic.permissionRows}
+                    getKey={permissionLogic.getPermissionKey} />
             </EpistoLabel>
         </LoadingFrame>
     );
---
+++ epistogram_frontend/src/static/EpistoIcons.tsx
@@ -0,0 +1,13 @@
+import { Delete } from '@mui/icons-material';
+import FiberNewIcon from '@mui/icons-material/FiberNew';
+import RemoveIcon from '@mui/icons-material/Remove';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+
+export const EpistoIcons = {
+    Delete: Delete,
+    DeleteOutline: DeleteOutlineIcon,
+    New: FiberNewIcon,
+    Minus: RemoveIcon,
+    ArrowCircleUp: ArrowCircleUpIcon
+};
