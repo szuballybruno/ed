eecab51f1fab5d3e63c580bfd3f6912c0490651b
Bence Manyoki <manyoki.bence@epistogram.com>
2022-06-24 08:36:27 +0200

    asd

--- epistogram_backend/src/sql/views/common/course_item_playlist_view.sql
+++ epistogram_backend/src/sql/views/common/course_item_playlist_view.sql
@@ -28,10 +28,7 @@ latest_course_items AS
 	SELECT 
 		civ.*,
 		(SELECT encode((civ.module_id || '@module')::bytea, 'base64')) module_code,
-		CASE WHEN civ.video_id IS NOT NULL
-			THEN (SELECT encode((civ.video_id || '@video')::bytea, 'base64')) 
-			ELSE (SELECT encode((civ.exam_id || '@exam')::bytea, 'base64'))
-		END item_code
+		(SELECT encode((civ.version_code || '@video')::bytea, 'base64')) item_code
 	FROM public.latest_course_version_view lcvi
 	
 	LEFT JOIN public.course_item_view civ
