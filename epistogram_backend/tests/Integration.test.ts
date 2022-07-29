6cfef9d851d0f9e89d38599b192c67936878ede0
Bence Manyoki <manyoki.bence@epistogram.com>
2022-07-29 16:11:09 +0200

    tests

--- epistogram_backend/tests/Integration.test.ts
+++ epistogram_backend/tests/Integration.test.ts
@@ -1,17 +1,12 @@
-// import { SQLConnectionService } from '../src/services/sqlServices/SQLConnectionService';
-// import { UserService } from '../src/services/UserService';
-// import { initServiceProvider } from '../src/startup/initApp';
-// import { ServiceProvider } from '../src/startup/servicesDI';
-
 import { UserService } from '../src/services/UserService';
-import { setupTest } from './base';
+import { setupTest as setupIntegrationTest } from './base';
 
-setupTest((getServiceProvider) => {
+setupIntegrationTest((getServiceProvider) => {
 
     /**
-     * User service
+     * test 1
      */
-     describe(UserService.name, () => {
+     describe('integration test 1', () => {
         it('is getting user 1', async () => {
 
             const user = await getServiceProvider()
@@ -21,46 +16,19 @@ setupTest((getServiceProvider) => {
             console.log(user);
         });
     });
+
+    /**
+     * test 2
+     */
+    describe('integration test 2', () => {
+        it('is doing something', async () => {
+
+            console.log('Something...');
+            // const user = await getServiceProvider()
+            //     .getService(UserService)
+            //     .getUserById(1 as any);
+
+            // console.log(user);
+        });
+    });
 });
-
-// const { getServiceProviderAsync } = initServiceProvider('');
-// let serviceProvider: ServiceProvider = null as any;
-
-// /**
-//  * --------------------- Init tests
-//  */
-// describe('init tests', () => {
-//     it('should init tests', async () => {
-//         serviceProvider = await getServiceProviderAsync();
-//     });
-// });
-
-// /**
-//  * User service
-//  */
-// describe(UserService.name, () => {
-//     it('is getting user 1', async () => {
-
-//         const user = await serviceProvider
-//             .getService(UserService)
-//             .getUserById(1 as any);
-
-//         console.log(user);
-//     });
-// });
-
-// /**
-//  * ----------------------- Destruct tests
-//  */
-// describe('Destruct tests', () => {
-//     it('should descruct tests', async () => {
-
-//         serviceProvider
-//             .getService(SQLConnectionService)
-//             .releaseConnectionClient();
-
-//         await serviceProvider
-//             .getService(SQLConnectionService)
-//             .endPoolAsync();
-//     });
-// });
