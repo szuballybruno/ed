909510cb2af85b7e1e024e25f50b27bd6209626b
Bence Manyoki <manyoki.bence@epistogram.com>
2022-08-02 10:27:56 +0200

    fix

--- epistogram_backend/src/sql/views/course_all_items_completed_view.sql
+++ epistogram_backend/src/sql/views/course_all_items_completed_view.sql
@@ -1,10 +1,12 @@
 SELECT 
 	sq.course_id,
+	sq.course_version_id,
 	sq.user_id
 FROM
 (
 	SELECT 
 		cipv.course_id,
+		cipv.course_version_id,
 		cipv.user_id,
 		COUNT(cipv.course_id) all_item_count,
 		SUM(CASE WHEN cipv.item_state = 'completed' THEN 1 ELSE 0 END) completed_count
@@ -12,7 +14,8 @@ FROM
 
 	GROUP BY 
 		cipv.course_id,
-		cipv.user_id
+		cipv.user_id,
+		cipv.course_version_id
 ) sq
 
 WHERE sq.completed_count = sq.all_item_count
