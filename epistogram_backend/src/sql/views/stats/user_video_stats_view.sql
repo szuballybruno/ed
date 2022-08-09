297fe8b141dc70f39e6778d56eca2bc079868d4c
Bence Manyoki <manyoki.bence@epistogram.com>
2022-08-09 09:25:29 +0200

    asd

--- epistogram_backend/src/sql/views/stats/user_video_stats_view.sql
+++ epistogram_backend/src/sql/views/stats/user_video_stats_view.sql
@@ -1,3 +1,63 @@
+WITH
+video_replays_cte AS
+(
+    SELECT
+        uvppv.video_id,
+        uvppv.user_id,
+        GREATEST(COUNT(*) - 1, 0)::int video_repetition_count
+    FROM public.user_video_practise_progress_view uvppv
+
+    WHERE uvppv.watch_percentage > 20
+    
+    GROUP BY
+        uvppv.user_id,
+        uvppv.video_id
+),
+last_3_quiz_answer_avg_cte AS
+(
+    SELECT
+        asv.user_id,
+        vv.video_id, 
+        AVG(ga.is_correct::int)::int average_correct_given_answer_count
+    FROM public.given_answer ga
+
+    LEFT JOIN public.answer_session_view asv
+    ON asv.answer_session_id = ga.answer_session_id
+
+    INNER JOIN public.video_version vv
+    ON vv.id = asv.video_version_id
+    
+    GROUP BY
+        vv.video_id, 
+        asv.user_id
+),
+avg_reaction_time_cte AS
+(
+    SELECT
+        uav.user_id,
+        uav.video_id,
+        AVG(uav.elapsed_seconds) avg_reaction_time
+    FROM public.user_answer_view uav
+    
+    WHERE uav.elapsed_seconds IS NOT NULL 
+    AND uav.video_id IS NOT NULL
+    
+    GROUP BY
+        uav.user_id,
+        uav.video_id
+),
+latest_playback_date_cte AS 
+(
+    SELECT
+        uvppv.user_id,
+        uvppv.video_id,
+        MAX(uvppv.creation_date) latest_playback_date
+    FROM public.user_video_practise_progress_view uvppv
+    
+    GROUP BY
+        uvppv.user_id,
+        uvppv.video_id
+)
 SELECT
     u.id user_id,
     cisv.video_id,
@@ -6,83 +66,31 @@ SELECT
     vd.title video_title,
     
     -- How much time the user spent with the video
-    (
-        SELECT
-            SUM(uvppv.playback_duration)::int
-        FROM public.user_video_practise_progress_view uvppv
-        WHERE uvppv.user_id = u.id
-        AND uvppv.video_id = cisv.video_id
-    ) total_spent_time_seconds,
+    uvpsv.total_playback_seconds total_spent_time_seconds,
     
     -- How many times the user replayed the video, 
     -- when the video is at least 20% watched
-    (
-        SELECT
-            CASE
-                WHEN COUNT(*) - 1 > 0
-                THEN COUNT(*) - 1
-                ELSE 0
-            END::int video_repetition_count
-        FROM public.user_video_practise_progress_view uvppv
-        WHERE uvppv.user_id = u.id
-        AND uvppv.video_id = cisv.video_id
-        AND uvppv.watch_percentage > 20
-    ) video_replays_count,
+    vrc.video_repetition_count video_replays_count,
     
     -- Is the video currently recommended for retry
     -- (When the video quiz questions has less than 66 percent success rate, 
     -- then it is recommended to watch again)
-    (
-        SELECT
-            CASE
-                WHEN COUNT(*) > 0
-                THEN TRUE
-                ELSE FALSE
-            END
-        FROM public.user_practise_recommendation_view uprv
-        WHERE uprv.user_id = u.id
-        AND uprv.video_version_id = vv.id
-        AND uprv.is_recommended_for_practise
-    ) is_recommended_for_retry,
+    uprv.is_recommended_for_practise is_recommended_for_retry,
 
     -- The average of the last 3 video quiz answers
-    (
-        SELECT
-            AVG(ga.is_correct::int)::int
-        FROM public.given_answer ga
-		
-		LEFT JOIN public.answer_session_view asv
-		ON asv.answer_session_id = ga.answer_session_id
-		AND asv.user_id = u.id
-		
-		LEFT JOIN public.video_version vv
-		ON vv.id = asv.video_version_id
-		AND vv.video_id = cisv.video_id
-    ) last_three_answer_average,
+    l3qaac.average_correct_given_answer_count last_three_answer_average,
 
     -- The average reaction time for the video quiz questions
-    (
-        SELECT
-            AVG(uav.elapsed_seconds)
-        FROM user_answer_view uav
-        WHERE uav.elapsed_seconds IS NOT NULL
-        AND uav.user_id = u.id
-        AND uav.video_id = cisv.video_id
-    ) average_reaction_time,
+    artc.avg_reaction_time average_reaction_time,
 
     -- When was the last time the user watched the video
-    (
-        SELECT
-            MAX(uvppv.creation_date)
-        FROM public.user_video_practise_progress_view uvppv
-        WHERE uvppv.user_id = u.id
-        AND uvppv.video_id = cisv.video_id
-    ) last_watch_time
+    lpdc.latest_playback_date last_watch_time
     
 FROM public.user u
 
-LEFT JOIN public.course_item_playlist_view cisv
-ON cisv.user_id = u.id
+INNER JOIN public.course_item_playlist_view cisv
+ON cisv.user_id = u.id 
+AND cisv.item_state = 'completed'
 
 LEFT JOIN public.video v
 ON v.id = cisv.video_id
@@ -100,13 +108,26 @@ LEFT JOIN public.video_playback_sample_view vpsv
 ON vpsv.video_version_id = vv.id
 AND vpsv.user_id = u.id
 
-WHERE cisv.item_state = 'completed'
-AND cisv.video_id IS NOT NULL
+LEFT JOIN public.user_video_playback_seconds_view uvpsv
+ON uvpsv.user_id = u.id
+AND uvpsv.video_id = vv.video_id
 
-GROUP BY
-    u.id,
-	vv.id,
-    cisv.video_id,
-    cisv.course_id,
-    vf.length_seconds,
-    vd.title
+LEFT JOIN video_replays_cte vrc
+ON vrc.user_id = u.id
+AND vrc.video_id = vv.video_id
+
+LEFT JOIN public.user_practise_recommendation_view uprv
+ON uprv.video_id = vv.video_id
+AND uprv.user_id = u.id
+
+LEFT JOIN last_3_quiz_answer_avg_cte l3qaac
+ON l3qaac.video_id = vv.video_id
+AND l3qaac.user_id = u.id
+
+LEFT JOIN avg_reaction_time_cte artc
+ON artc.video_id = vv.video_id
+AND artc.user_id = u.id
+
+LEFT JOIN latest_playback_date_cte lpdc
+ON lpdc.video_id = vv.video_id
+AND lpdc.user_id = u.id
