3cf805a26c64dd0b7b1e2cc604cb42640c9f3174
Bence Manyoki <manyoki.bence@epistogram.com>
2022-08-11 15:05:08 +0200

    seed courses

--- epistogram_backend/src/sql/seed/seed_course_access_bridge.ts
+++ epistogram_backend/src/sql/seed/seed_course_access_bridge.ts
@@ -13,26 +13,31 @@ export const getCourseAccessBridgeSeedData = (
             courseId: courses.course_obs.id,
             userId: null
         },
-        pw_course_pp: {
-            companyId: companies.PCWorld.id,
-            courseId: courses.course_powerPoint.id,
-            userId: null
-        },
-
-        // episto
         episto_course_obs: {
-            companyId: companies.EpistoGram.id,
+            companyId: companies.PCWorld.id,
             courseId: courses.course_obs.id,
             userId: null
         },
         episto_course_linkedin: {
-            companyId: companies.EpistoGram.id,
+            companyId: companies.PCWorld.id,
             courseId: courses.course_linked_in.id,
             userId: null
         },
         episto_course_ads: {
-            companyId: companies.EpistoGram.id,
+            companyId: companies.PCWorld.id,
             courseId: courses.course_google_ads.id,
+            userId: null
+        },
+
+        // episto
+        pw_course_pp: {
+            companyId: companies.EpistoGram.id,
+            courseId: courses.course_powerPoint.id,
+            userId: null
+        },
+        pw_course_word: {
+            companyId: companies.EpistoGram.id,
+            courseId: courses.course_word.id,
             userId: null
         },
         episto_course_excel: {
