c11be7d36a0f72f00eaeecc83a5935e39656af7c
Bence Manyoki <manyoki.bence@epistogram.com>
2022-11-24 16:41:18 +0100

    asd

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -10,6 +10,72 @@ import { ServiceProviderInitializator } from './startup/initApp';
 import { initTurboExpress } from './startup/instatiateTurboExpress';
 import { XTurboExpressListener } from './turboImplementations/XTurboExpressListener';
 
+
+// RPC
+type XRPCActionResult<TName, TAction> = {
+    name: TName;
+    action: TAction;
+};
+
+type XRPCAction = () => XRPCActionResult<any, any>;
+
+type XRPCController<T> = {
+    [K in keyof T]: XRPCAction
+}
+
+const fn = <TName extends Readonly<string>, TAction>(name: TName, fn: TAction): () => XRPCActionResult<TName, TAction> => {
+
+    return null as any;
+}
+
+class AsdController implements XRPCController<AsdController> {
+
+    constructor() {
+
+    }
+
+    getAsd = fn('get-asd', (param1: string) => {
+
+        return {
+            blah: 'fasz',
+            ba: param1
+        };
+    })
+}
+
+class Asd2Controller implements XRPCController<Asd2Controller> {
+
+    constructor() {
+
+    }
+
+    getLol = fn('get-lol', (param1: string) => {
+
+        return {
+            ny: 'fasz',
+            ba: param1
+        };
+    })
+}
+
+type ControllersType = AsdController & Asd2Controller;
+
+type ApiManifestType = {
+    [K in keyof ControllersType as ReturnType<ControllersType[K]>['name']]: {
+        paramsType: Parameters<ReturnType<ControllersType[K]>['action']>,
+        returnType: ReturnType<ReturnType<ControllersType[K]>['action']>
+    };
+}
+
+const call = <TActionKey extends keyof ApiManifestType>(
+    action: TActionKey,
+    ...args: ApiManifestType[TActionKey]['paramsType']): ApiManifestType[TActionKey]['returnType'] => {
+
+    return null as any;
+}
+
+const res = call('get-lol', 'lol');
+
 jsExtensions.initJsExtensions();
 
 const rootDir = dirname(fileURLToPath(import.meta.url));
