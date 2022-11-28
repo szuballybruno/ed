134fe716ed8be22684a82578a681d492f3281ffe
Bence Manyoki <manyoki.bence@epistogram.com>
2022-11-28 19:53:54 +0100

    fix?

--- epitest/run_migen.ps1
+++ epitest/run_migen.ps1
@@ -1,3 +1,5 @@
-docker run -v ${PWD}/migen_out:/app/packages/util-script-producer/out migen
+docker run `
+    -v ${PWD}/migen_out:/app/packages/util-script-producer/out `
+    migen
 
 # @REM docker cp container-id:/app/packages/util-script-producer/out ./migen_out/script.sql
