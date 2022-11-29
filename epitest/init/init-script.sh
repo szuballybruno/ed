089cbfff70638696620d0bdfd617fff2e3d18041
Bence Manyoki <manyoki.bence@epistogram.com>
2022-11-29 17:19:37 +0100

    asd

--- epitest/init/init-script.sh
+++ epitest/init/init-script.sh
@@ -1,7 +1,7 @@
 echo "---------- Restoring DB..."
-psql -U dev_service_user -d localhostDB -f ./../../init/database-backup.sql -q
+psql -U dev_service_user -d localhostDB -f ./../../init/database-backup.sql --quiet
 echo "---------- Restoring DB done."
 
 echo "---------- Migrating DB..."
-psql -U dev_service_user -d localhostDB -f ./../../init/migration-script.sql -q
+psql -U dev_service_user -d localhostDB -f ./../../init/migration-script.sql --quiet
 echo "---------- Migrating DB done."
