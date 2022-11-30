3a7177de148e009492b09d27af66636ee42dda67
Bence Manyoki <manyoki.bence@epistogram.com>
2022-11-30 13:17:12 +0100

    hejj

--- epitest/init/init-script.sh
+++ epitest/init/init-script.sh
@@ -1,11 +1,7 @@
-PGOPTIONS="-c client_min_messages=error"
-
 echo "---------- Restoring DB..."
-psql -U dev_service_user -d localhostDB -f ./../../init/database-backup.sql --quiet -o ./../../init/logs_backup.log -v ON_ERROR_STOP=1 ./../../init/logs_backup2.log
-# --quiet -P pager=off -o ./../../init/logs_backup.log
+psql -U dev_service_user -d localhostDB -f ./../../init/database-backup.sql --quiet -v ON_ERROR_STOP=1 # -o ./../../init/logs_backup.log > ./../../init/logs_backup2.log
 echo "---------- Restoring DB done."
 
 echo "---------- Migrating DB..."
-psql -U dev_service_user -d localhostDB -f ./../../init/migration-script.sql --quiet -o ./../../init/logs_migration.log -v ON_ERROR_STOP=1 &>> ./../../init/logs_migration2.log
-# --quiet -P pager=off -o ./../../init/logs_migration.log
+psql -U dev_service_user -d localhostDB -f ./../../init/migration-script.sql --quiet -v ON_ERROR_STOP=1 # -o ./../../init/logs_migration.log > ./../../init/logs_migration2.log
 echo "---------- Migrating DB done."
