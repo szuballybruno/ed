1e28114baccee284e022fbd012e2e6173bdbdf6b
Bence Manyoki <manyoki.bence@epistogram.com>
2022-11-30 14:02:12 +0100

    no error stop on backup

--- epitest/init/init-script.sh
+++ epitest/init/init-script.sh
@@ -1,5 +1,5 @@
 echo "---------- Restoring DB..."
-psql -U dev_service_user -d localhostDB -f ./../../init/database-backup.sql --quiet -v ON_ERROR_STOP=1 # -o ./../../init/logs_backup.log > ./../../init/logs_backup2.log
+psql -U dev_service_user -d localhostDB -f ./../../init/database-backup.sql --quiet # -v ON_ERROR_STOP=1 # -o ./../../init/logs_backup.log > ./../../init/logs_backup2.log
 echo "---------- Restoring DB done."
 
 echo "---------- Migrating DB..."
