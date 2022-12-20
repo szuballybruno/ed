a1bb02934409518e2afe3332c5c8ecbd16c422c4
Bence Manyoki <manyoki.bence@epistogram.com>
2022-12-20 17:52:16 +0100

    progress

--- scripts/symlink.ps1
+++ scripts/symlink.ps1
@@ -1,1 +1,1 @@
-New-Item -ItemType SymbolicLink -Path "${PWD}/../thinkhub-xlib" -Target "${PWD}/../../thinkhub-xlib"
+New-Item -ItemType SymbolicLink -Path "${PWD}/../thinkhub-xlib" -Target "${PWD}/../../thinkhub-xlib/packages"
