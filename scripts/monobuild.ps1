3f9ee95c077a782eab4703b095365a12d286e655
bencemanyoki <manyoki.bence@epistogram.com>
2022-12-29 16:28:22 +0100

    asd

--- scripts/monobuild.ps1
+++ scripts/monobuild.ps1
@@ -5,25 +5,31 @@ param (
 
 echo "Build deps: ${buiddeps}"
 
+$root = Resolve-Path "${PSScriptRoot}/../../"
+$episto_root = Resolve-Path "${root}/epistogram/"
+
+echo "Root: ${root}"
+echo "Episto root: ${episto_root}"
+
 if ($builddeps) 
 {
     # build monodeps
     echo "------ Building monodeps..."
-    docker build ../../ --file ./../docker/monodeps.Dockerfile --tag monodeps
+    docker build ${root} --file ${episto_root}/docker/monodeps.Dockerfile --tag monodeps
 }
 
 # build monosrc
 echo "------ Building monosrc..."
-docker build ../../ --file ./../docker/monosrc.Dockerfile --tag monosrc
+docker build "${root}" --file ${episto_root}/docker/monosrc.Dockerfile --tag monosrc
 
 # build server
 echo "------ Building server..."
-docker build ../../ --file ./../packages/server-api/server.Dockerfile --tag server
+docker build "${root}" --file ${episto_root}/packages/server-api/server.Dockerfile --tag server
 
 # build client
 echo "------ Building client..."
-docker build ../../ --file ./../packages/frontend/client.Dockerfile --build-arg ENVIRONMENT_NAME=${client_env} --tag client
+docker build "${root}" --file ${episto_root}/packages/frontend/client.Dockerfile --build-arg ENVIRONMENT_NAME=${client_env} --tag client
 
 # build tests-client
 echo "------ Building tests-client..."
-docker build ../../ --file ./../packages/tests-client/testsclient.Dockerfile --tag tests-client
+docker build "${root}" --file ${episto_root}/packages/tests-client/testsclient.Dockerfile --tag tests-client
